a41527c19db634c932d6227c4178ab72259aa7c6
DatsOlena <datsolena@gmail.com>
2025-08-30 21:26:36 +0200

    Add currentTodo exports

--- src/features/currentTodo.ts
+++ src/features/currentTodo.ts
@@ -1,10 +1,18 @@
 import { createSlice } from '@reduxjs/toolkit';
 import { Todo } from '../types/Todo';
+import _ from 'cypress/types/lodash';
 
 const initialState = null as Todo | null;
 
 export const currentTodoSlice = createSlice({
   name: 'currentTodo',
   initialState,
-  reducers: {},
+  reducers: {
+    setCurrentTodo: (_, action) => {
+      return action.payload;
+    }
+  },
 });
+
+export const { setCurrentTodo } = currentTodoSlice.actions;
+export default currentTodoSlice.reducer;
