a05e78926150699ecd15ffc681370ed476bfc9d0
DatsOlena <datsolena@gmail.com>
2025-09-01 18:37:55 +0200

    Fix test errors

--- src/components/TodoFilter/TodoFilter.tsx
+++ src/components/TodoFilter/TodoFilter.tsx
@@ -16,6 +16,11 @@ export const TodoFilter: React.FC = () => {
     dispatch(setFilter({ ...filter, query: event.target.value }));
   };
 
+  const handleClear = () => {
+    dispatch(setFilter({ ...filter, query: '' }));
+    document.querySelector('input')!.value = '';
+  };
+
   return (
     <form
       className="field has-addons"
@@ -45,11 +50,13 @@ export const TodoFilter: React.FC = () => {
 
         <span className="icon is-right" style={{ pointerEvents: 'all' }}>
           {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
-          <button
-            data-cy="clearSearchButton"
-            type="button"
-            className="delete"
-          />
+          {filter.query.length > 0 && (
+            <button
+              onClick={handleClear}
+              data-cy="clearSearchButton"
+              type="button"
+              className="delete"
+            />)}
         </span>
       </p>
     </form>
--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -9,6 +9,7 @@ export const TodoList: React.FC = () => {
   const todos = useAppSelector(state => state.todos);
   const dispatch = useDispatch();
   const { query, status } = useAppSelector(state => state.filter);
+  const currentTodo = useAppSelector(state => state.currentTodo);
 
   const handleSelectTodo = (todo: Todo) => {
     dispatch(setCurrentTodo(todo));
@@ -35,60 +36,64 @@ export const TodoList: React.FC = () => {
 
   return (
     <>
-      <p className="notification is-warning">
-        There are no todos matching current filter criteria
-      </p>
+      {
+        filteredTodos.length > 0 ? <table className="table is-narrow is-fullwidth">
+          <thead>
+            <tr>
+              <th>#</th>
 
-      <table className="table is-narrow is-fullwidth">
-        <thead>
-          <tr>
-            <th>#</th>
+              <th>
+                <span className="icon">
+                  <i className="fas fa-check" />
+                </span>
+              </th>
 
-            <th>
-              <span className="icon">
-                <i className="fas fa-check" />
-              </span>
-            </th>
-
-            <th>Title</th>
-            <th> </th>
-          </tr>
-        </thead>
-
-        <tbody>
-          {filteredTodos.map(todo => (
-            <tr data-cy="todo" key={todo.id}>
-              <td className="is-vcentered">{todo.id}</td>
-              <td className="is-vcentered">
-                {todo.completed && (
-                  <span className="icon" data-cy="iconCompleted">
-                    <i className="fas fa-check" />
-                  </span>
-                )}
-              </td>
-              <td className="is-vcentered is-expanded">
-                <p
-                  className={` ${todo.completed ? 'has-text-success' : 'has-text-danger'}`}
-                >
-                  {todo.title}
-                </p>
-              </td>
-              <td className="has-text-right is-vcentered">
-                <button
-                  data-cy="selectButton"
-                  className="button"
-                  type="button"
-                  onClick={() => handleSelectTodo(todo)}
-                >
-                  <span className="icon">
-                    <i className="far fa-eye" />
-                  </span>
-                </button>
-              </td>
+              <th>Title</th>
+              <th> </th>
             </tr>
-          ))}
-        </tbody>
-      </table>
+          </thead>
+
+          <tbody>
+            {filteredTodos.map(todo => (
+              <tr data-cy="todo" key={todo.id}>
+                <td className="is-vcentered">{todo.id}</td>
+                <td className="is-vcentered">
+                  {todo.completed && (
+                    <span className="icon" data-cy="iconCompleted">
+                      <i className="fas fa-check" />
+                    </span>
+                  )}
+                </td>
+                <td className="is-vcentered is-expanded">
+                  <p
+                    className={` ${todo.completed ? 'has-text-success' : 'has-text-danger'}`}
+                  >
+                    {todo.title}
+                  </p>
+                </td>
+                <td className="has-text-right is-vcentered">
+                  <button
+                    data-cy="selectButton"
+                    className="button"
+                    type="button"
+                    onClick={() => handleSelectTodo(todo)}
+                  >
+                    <span className="icon">
+                      <i
+                        className={`far fa-eye${currentTodo === todo ? '-slash' : ''}`}
+                      />
+                    </span>
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table> : <p className="notification is-warning">
+          There are no todos matching current filter criteria
+        </p>
+      }
+
+
     </>
   );
 };
